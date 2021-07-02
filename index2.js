d1559a78905829590a4e41cdfcabc76dca6d9e0e
Sagar Panwar <85165483+sgrpnwr@users.noreply.github.com>
2021-07-02 23:17:28 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,9 +1,37 @@
 // alert("Hello!")
-var songs = ["Alag_Aasmaan.mp3", "Clocks.mp3", "Gully_Boy.mp3", "LeapOfFaith.mp3", "Brown_Mundey.mp3"];
+var songs = [
+  {
+    name:"Alag_Aasmaan.mp3",
+    artist:"Anuv Jain",
+    title:"Alag Aasmaan"
+  },
+
+  {
+    name:"Clocks.mp3",
+    artist:"Coldplay",
+    title:"Clocks"
+  },
+  {
+    name:"Gully_Boy.mp3",
+    artist:"MIDIval Punditz, Raghu Dixit",
+    title:"Train Song"
+  },
+  {
+    name:"LeapOfFaith.mp3",
+    artist:"Christopher",
+    title:"Leap of Faith"
+  },
+  {
+    name:"Brown_Mundey.mp3",
+    artist:"AP Dhillon",
+    title:"Brown Munde"
+  },
+
+  ];
 
 let isPlaying=false;
 var i=0;
-var song=new Audio(songs[i]);
+var song=new Audio(songs[i].name);
 $(".play-btn").click(function(){
 
 if(isPlaying===false){
@@ -18,16 +46,22 @@ else if(isPlaying===true){
 $(".next-btn").click(function(){
 pauseSong();
 i=(i+1)%songs.length;
-song=new Audio(songs[i]);
-$("img").attr("src",songs[i].replace("mp3","jpeg"));
+song=new Audio(songs[i].name);
+$("img").attr("src",songs[i].name.replace("mp3","jpeg"));
+$(".track").html(songs[i].title);
+$(".artist").html(songs[i].artist)
+
 playSong(song);
 
 });
 $(".prev-btn").click(function(){
 pauseSong();
 i=(i-1+songs.length)%songs.length;
-song=new Audio(songs[i]);
-$("img").attr("src",songs[i].replace("mp3","jpeg"));
+song=new Audio(songs[i].name);
+$("img").attr("src",songs[i].name.replace("mp3","jpeg"));
+$(".track").html(songs[i].title)
+$(".artist").html(songs[i].artist)
+
 playSong(song);
 
 
@@ -41,8 +75,10 @@ function playSong(){
   song.play();
   $(".play-btn").removeClass('far fa-play-circle');
   $(".play-btn").addClass('far fa-pause-circle');
-  $(".play-btn").css("background-color","red");
+  $(".play-btn").css("background-color","#9FE6A0");
   $("body").addClass("bg");
+
+
 }
 function pauseSong(){
 
@@ -51,7 +87,7 @@ console.log("Song is Paused");
 song.pause();
 $(".play-btn").removeClass('far fa-pause-circle');
 $(".play-btn").addClass('far fa-play-circle');
-$(".play-btn").css("background-color","yellow");
+$(".play-btn").css("background-color","#F5A962");
 $("body").removeClass("bg");
 
 
